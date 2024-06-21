4a8424d56706aa4f7c27238e7a34b22cd770d33b
ACEThreat <shanestewartcyber@gmail.com>
2024-06-21 12:15:23 -0400

    Update Sysmon-OneClick.ps1
    Added licensing

--- Sysmon-OneClick.ps1
+++ Sysmon-OneClick.ps1
@@ -9,6 +9,20 @@
     Creation Date:  5/17/23 ||| Update: 6/11/24
     Purpose/Change: Updated for public release.
    
+    Copyright 2024 ACEThreat
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   
 $binary = [convert]::ToBase64String((Get-Content -path "FILEPATH\FILENAME.ZIP" -Encoding byte))
 $binary | Out-File ("FILEPATH\FILENAME.txt")
 .EXAMPLE
