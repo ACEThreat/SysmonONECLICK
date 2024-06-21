715a36abc1c965257b5f6338a68e473ba3fc47fb
ACEThreat <shanestewartcyber@gmail.com>
2024-06-21 12:14:48 -0400

    Update Python-B64-Encoder.py
    Added licensing

--- Python-B64-Encoder.py
+++ Python-B64-Encoder.py
@@ -1,3 +1,21 @@
+# This program enables b64 encoding files to paste into other scripts or developments (Such as Sysmon-OneClick https://github.com/ACEThreat/SysmonONECLICK)
+# Copyright 2024 ACEThreat
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# Created by Git: @ACEThreat
+# LinkedIn: @snsl 
+####################################################################################################################
 import tkinter as tk
 from tkinter import filedialog, messagebox
 import base64
